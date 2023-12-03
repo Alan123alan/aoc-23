096d76daf6e7c548a280c5545fcda905e96ddaf4
Alan123alan <alancastellanos2@hotmail.com>
2023-12-03 09:52:35 -0600

    solved aoc day 2 part 1

---
+++ day2/src/main.rs
@@ -0,0 +1,107 @@
+use std::fs;
+// use std::collections::HashMap;
+
+fn read_input(path:&str){
+    let input = fs::read_to_string(path).expect("Should have worked");
+    let lines: Vec<&str> = input.split("\n").collect();
+    //an iterator is lazy, you need to consume it with collect
+    let game_records: Vec<Vec<&str>> = lines.iter().map(|line| line.split(":").collect()).collect();
+    let game_ids: Vec<i32> = game_records.iter().map(|game_record| game_record[0].split_once(" ").unwrap().1.parse().unwrap()).collect();
+    let cube_subsets: Vec<&str> = game_records.iter().map(
+        |game_record| game_record[1]
+    ).collect();
+    let cube_subsets: Vec<Vec<&str>> = cube_subsets.iter().map(
+        |cube_subset| cube_subset.split(";").collect()
+    ).collect();
+    let cube_subsets: Vec<Vec<Vec<&str>>> = cube_subsets.iter().map(
+        |cube_subset| cube_subset.iter().map(
+            |colors|colors.split(",").collect()
+        ).collect()
+    ).collect();
+    let cube_subsets: Vec<Vec<Vec<(&str, &str)>>> = cube_subsets.iter().map(
+        |cube_subset| cube_subset.iter().map(
+            |colors| colors.iter().map(
+                |color| color.trim().split_once(" ").unwrap()
+            ).collect()
+        ).collect()
+    ).collect();
+    let mut possible_games_sum = 0;
+    for index in 0..game_ids.len(){
+        println!("game_id:{}",game_ids[index]);
+        let mut is_game_possible = true;
+        // let mut red = 0;
+        // let mut green = 0;
+        // let mut blue = 0;
+        for colors in &cube_subsets[index]{
+            for color in colors{
+                let number_of_cubes:i32= color.0.parse().unwrap();
+                let cube_color = color.1;
+                // println!("{number_of_cubes}:{cube_color}");
+                is_game_possible = match cube_color {
+                    "red"=> number_of_cubes <= 12,
+                    "green"=> number_of_cubes <= 13,
+                    "blue"=> number_of_cubes <= 14,
+                    _ => false
+                };
+                if !is_game_possible{
+                    break;
+                }
+            }
+            if !is_game_possible{
+                break;
+            }
+        }
+        // println!("red:{red}");
+        // println!("green:{green}");
+        // println!("blue:{blue}");
+        if is_game_possible{
+            println!("{}",game_ids[index]);
+            possible_games_sum += game_ids[index];
+        }
+    }
+    println!("{possible_games_sum}");
+    // for cube_subset in cube_subsets{
+    //     for colors in cube_subset{
+    //         for color in colors{
+    //             let number_of_cubes = color.0;
+    //             let cube_color = color.1;
+    //             println!("{number_of_cubes}:{cube_color}");
+    //         }
+    //     }
+    // }
+    // println!("{:?}", game_records);
+    // for game_record in game_records.iter(){
+    //     let game_id = game_record[0];
+    //     let cubes_subset = game_record[1];
+    // }
+    // println!("{:?}", game_ids);
+    // println!("{:?}", cube_subsets);
+    // let mut game_hashmap:HashMap<&str, &HashMap<&str,&str>> = HashMap::new();
+    // for line in lines.iter(){
+    //     let game_record: Vec<&str> = line.split(":").collect();
+    //     let game_id= game_record[0];
+    //     println!("{}",game_id);
+    //     let cubes_subset: Vec<&str> = game_record[1].split(";").collect();
+    //     let mut color_hashmap:HashMap<&str,&str> = HashMap::new();
+    //     for subset in cubes_subset.iter(){ 
+    //         let colors: Vec<&str> = subset.split(",").collect();
+    //         for color in colors.iter(){
+    //             let key_val: Vec<&str> = color.trim().split(" ").collect();
+    //             color_hashmap.insert(key_val[1], key_val[0]);
+    //             println!("{:?}",key_val);
+    //         }
+    //         game_hashmap.insert(game_id, &color_hashmap);
+    //         println!("{subset}");
+    //         println!("{:?}",colors);
+    //     }
+    //     // println!("{:?}", line);
+    //     // println!("{:?}", game_record);
+    //     // println!("{}", game_id);
+    //     // println!("{:?}", cubes_subset);
+    // }
+    // println!("{:?}",lines);
+}
+
+fn main() {
+    read_input("./src/input_day2_p1.txt")
+}
