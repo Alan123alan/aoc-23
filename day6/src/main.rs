3c61669506055f0a6611fd5f721a2366b1b320fc
Alan123alan <alancastellanos2@hotmail.com>
2023-12-18 15:52:36 -0700

    solved day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,7 +1,6 @@
 use std::fs;
 
-fn main() {    
-    let path = "./src/day6_p1.txt";
+fn day6_p1(path:&str){
     let input: Vec<String> = fs::read_to_string(path).expect("Failed to read file.").lines().map(|line| line.to_string()).collect();
     let time_and_distance: Vec<(&str, &str)>= input.iter().map(|line| line.split_once(":").unwrap()).collect();
     let mut time_limits: Vec<i32> = Vec::new();
@@ -44,7 +43,51 @@ fn main() {
         counts.push(count);
     }
     println!("{:?}", counts.iter().fold(1, |acc, el| acc * el));
+}
 
-    // println!("{:?}",input);
-    // println!("{:?}",time_and_distance);
+fn day6_p2(path:&str){
+    let input: Vec<String> = fs::read_to_string(path).expect("Failed to read file.").lines().map(|line| line.to_string()).collect();
+    let times_and_distances: Vec<(&str, &str)>= input.iter().map(|line| line.split_once(":").unwrap()).collect();
+    let mut time_limit: i64 = 0;
+    let mut distance_to_beat: i64= 0;
+    for (label, values) in times_and_distances.iter(){
+        let mut value = values.to_string();
+        value.retain(|ch| !ch.is_whitespace());
+        match *label {
+            "Time" => time_limit = value.parse().unwrap(),
+            "Distance" => distance_to_beat = value.parse().unwrap(),
+            _ => ()
+        }
+        println!("{} : {}", label, value);
+    }
+    println!("{} : {}", time_limit, distance_to_beat);
+    // let mut results: Vec<Vec<i32>> = Vec::new();
+    let mut results: Vec<i64> = Vec::new();
+    for milliseconds_button_pressed in 0..(time_limit + 1){
+        let distance_travelled = (time_limit - milliseconds_button_pressed) * milliseconds_button_pressed;
+        // println!("{:?}", distance_travelled);
+        if distance_travelled > distance_to_beat{
+            results.push(distance_travelled);
+        }
+    }
+        // results.push(result);
+    println!("{:?}", results.len());
+    // let mut counts: Vec<i32> = Vec::new();
+    // for (index, distance_to_beat)in distances_to_beat.iter().enumerate(){
+    //     let mut count = 0;
+    //     for distance_travelled in results[index].iter(){
+    //         if distance_travelled > distance_to_beat{
+    //             count += 1;
+    //         }
+    //     }
+    //     counts.push(count);
+    // }
+    // println!("{:?}", counts.iter().fold(1, |acc, el| acc * el));
+
+}
+
+fn main() {    
+    let path = "./src/day6_p1.txt";
+    // day6_p1(path);
+    day6_p2(path);
 }
