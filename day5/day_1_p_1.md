97aabef0c3dcc20b96b6126873c690bada324688
Alan123alan <alancastellanos2@hotmail.com>
2023-12-17 20:24:56 -0700

    trying to reduce execution time of day 5 part 2

--- day5/day_1_p_1.md
+++ day5/day_1_p_1.md
@@ -92,3 +92,32 @@ update the source to the destination mapping obtained
 Does this work in practice?
 
 
+Part 2
+
+The values on the initial seeds: line come in pairs. Within each pair, the first value is the start of the range and the second value is the length of the range. So, in the first line of the example above:
+
+seeds: 79 14 55 13
+This line describes two ranges of seed numbers to be planted in the garden. The first range starts with seed number 79 and contains 14 values: 79, 80, ..., 91, 92. The second range starts with seed number 55 and contains 13 values: 55, 56, ..., 66, 67.
+
+Now, rather than considering four seed numbers, you need to consider a total of 27 seed numbers.
+
+In the above example, the lowest location number can be obtained from seed number 82, which corresponds to soil 84, fertilizer 84, water 84, light 77, temperature 45, humidity 46, and location 46. So, the lowest location number is 46.
+
+Consider all of the initial seed numbers listed in the ranges on the first line of the almanac. What is the lowest location number that corresponds to any of the initial seed numbers?
+
+Expanding the seeds also makes this solution incredibly slow, slower than my p1 v1 solution.
+
+What to do instead of expanding into individual seeds? can I use the range in some wa as the sped up p1 V2?
+
+if you take the limits of the seeds range, 79..92, how can you define with only the range limits, which is the lowest seed?
+
+
+#### Reducing amount of computations to find the correct mapping
+what about reducing the amount of computations to get the same result?
+
+
+#### Eliminating pusshing the locations into an array
+Does it improve the execution time? No
+Does it at least passes from the first seed group whitin 5 mins? No
+
+
--- day5/src/main.rs
+++ day5/src/main.rs
@@ -10,38 +10,46 @@ fn main() {
         .map(|(label, values)| (label.trim().to_owned(), values.trim().lines().map(|line| line.split_whitespace().map(|num| num.to_owned()).collect::<Vec<String>>()).collect::<Vec<Vec<String>>>()))
         .collect();
     let(_, seeds) = input[0].clone();
-    println!("seeds {:?}", seeds.first().unwrap());
-    let mut sources: Vec<i64> = Vec::new();
-    for seed in seeds.first().unwrap(){
-        let mut source: i64 = seed.parse().unwrap();
-        println!("Source: {:?}", source);
-        for (label, maps) in input[1..].iter(){
-            println!("{} {:?}", label, maps);
-            for map in maps.iter(){
-                println!("{} {} {}", map[0], map[1], map[2]);
-                let map_destination: i64= map[0].clone().parse().unwrap();
-                let map_source: i64 = map[1].clone().parse().unwrap();
-                let map_range: i64 =map[2].clone().parse().unwrap();
-                // let mut destination_expansion: Vec<i64> = Vec::new();
-                // let mut source_expansion: Vec<i64> = Vec::new();
-                // println!("Destination:{}, Source:{}, range:{}", map_destination, map_source, map_range);
-                // for i in 0..map_range{
-                //     destination_expansion.push(map_destination+i);
-                //     source_expansion.push(map_source+i);
-                // }
-                // if source_expansion.contains(&source){
-                //     source = destination_expansion[source_expansion.iter().position(|el| *el == source ).unwrap()];
-                //     break;
-                // }
-                if source >= map_source && source <= map_source + map_range - 1{
-                    source = map_destination + (source - map_source);
-                    break;
-                }
-                println!("{}{}{}", map_destination, map_source, map_range);
-            }
-        }
-        sources.push(source);
+    let mut seeds_expansion: Vec<Vec<i64>> = Vec::new();
+    let len = seeds.first().unwrap().len();
+    for index in (0..len).into_iter().step_by(2){
+        println!("{}", index);
+        let start: i64 = seeds.first().unwrap()[index].parse().unwrap(); 
+        let range: i64 = seeds.first().unwrap()[index+1].parse().unwrap();
+        let end: i64 = start + range - 1;
+        println!("start: {}, range: {}, end: {}", start, range, end);
+        seeds_expansion.push((start..end).into_iter().collect())
+
     }
-    println!("Locations {:?}", sources);
-    println!("Nearest location {:?}", sources.iter().min().unwrap());
+    // println!("seeds_expansion {:?}", seeds_expansion);
+    println!("seeds_expansion length {:?}", seeds_expansion.len());
+    println!("seeds_expansion first expansion length {:?}", seeds_expansion[0].len());
+    // println!("seeds {:?}", seeds.first().unwrap());
+    // let mut sources: Vec<i64> = Vec::new();
+    // for seed in seeds.first().unwrap(){
+    for (current_expansion, seed_expansion) in seeds_expansion.iter().enumerate(){
+        println!("{}", current_expansion);
+        for seed in seed_expansion{
+            // let mut source: i64 = seed.parse().unwrap();
+            let mut source: i64 = *seed;
+            // println!("Source: {:?}", source);
+            for (label, maps) in input[1..].iter(){
+                // println!("{} {:?}", label, maps);
+                for map in maps.iter(){
+                    // println!("{} {} {}", map[0], map[1], map[2]);
+                    let map_destination: i64= map[0].clone().parse().unwrap();
+                    let map_source: i64 = map[1].clone().parse().unwrap();
+                    let map_range: i64 =map[2].clone().parse().unwrap();
+                    if source >= map_source && source <= map_source + map_range - 1{
+                        source = map_destination + (source - map_source);
+                        break;
+                    }
+                            // println!("{}{}{}", map_destination, map_source, map_range);
+                }
+            }
+            // sources.push(source);
+        }
+    }
+    // println!("Locations {:?}", sources);
+    // println!("Nearest location {:?}", sources.iter().min().unwrap());
 }
