ab8d3a87440c1b4998b757523d1693417328b038
Alan123alan <alancastellanos2@hotmail.com>
2023-12-29 12:50:00 -0700

    solved advent of code part 1 for day 9

---
+++ day9/src/main.rs
@@ -0,0 +1,35 @@
+use std::fs;
+
+fn recurse(ref_serie: &Vec<i32>)->i32{
+    let mut diff_serie: Vec<i32> = Vec::new();
+    let mut current = 0;
+    let mut next = 1;
+    while next < ref_serie.len(){   
+        diff_serie.push(ref_serie[next as usize] - ref_serie[current as usize]);
+        current += 1;
+        next += 1;
+    }
+    println!("{:?}", diff_serie);
+    // if !diff_serie.iter().all(|diff| *diff == 0){
+    if diff_serie.iter().all(|diff| *diff == 0){
+        // recurse(&diff_serie);
+        return 0;
+    }
+    return diff_serie.iter().last().unwrap() + recurse(&diff_serie);
+}
+
+fn main() {
+    // let path = "src/test_p1.txt";
+    let path = "src/p1.txt";
+    let input: Vec<String> = fs::read_to_string(path).expect("Error reading file.").lines().map(|line| line.to_string()).collect();
+    let series: Vec<Vec<i32>> = input.iter().map(|line| line.split_whitespace().map(|num| num.parse().unwrap()).collect()).collect();
+    println!("{:?}", input);
+    println!("{:?}", series);
+    let mut total = 0;
+    for serie in series.iter(){
+        let return_val = serie.iter().last().unwrap() + recurse(serie);
+        total += return_val;
+        println!("{:?}", return_val);
+    }
+    println!("{:?}", total);
+}
